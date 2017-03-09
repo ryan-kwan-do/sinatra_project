665036e993be9fa25b5941f1463d04cdb1b1c99c
Ryan Armstrong <hello@ryanarmstrong.me>
2017-03-09 09:07:30 +0100

    fixed non-persistence with game variable

--- lib/web_games.rb
+++ lib/web_games.rb
@@ -2,13 +2,14 @@ require 'sinatra'
 require_relative 'web_games/guessing'
 #require 'sinatra/reloader' if development? 
 
+use Rack::Session::Pool, :expire_after => 2592000
+
 get '/' do
   erb :index, layout: :layout
 end
 
-
-GAME = GuessingGame::Game.new
-number = GAME.number
 get '/guessing' do
+  session[:game] ||= GuessingGame::Game.new
+  number = session[:game].number
   erb :guessing, :locals => {:number => number}, layout: :layout
 end
